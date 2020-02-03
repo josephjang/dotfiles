375d68af23a0c1ba9e636d54b619d52c77aef0b7
Jeongho Jang <josephjang@gmail.com>
2020-02-03 14:27:44 +0900

    Exclude .gitmodules when sync

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ git submodule init
 git submodule update
 
 function doIt() {
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" \
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude .gitmodules \
     --include ".**" \
     --archive --verbose . ~
 }
