427c4d13eb711884599de2a67c25a368013b6796
Jeongho Jang <josephjang@gmail.com>
2022-12-14 19:47:26 +0900

    Bootstrap script shows dry run result before prompting

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,19 +6,27 @@ git pull
 git submodule init
 git submodule update
 
-function doIt() {
+function dryRun() {
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude .gitmodules \
+    --dry-run \
+    --include ".**" \
+    --archive --verbose . ~
+}
+
+function run() {
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude .gitmodules \
     --include ".**" \
     --archive --verbose . ~
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt
+  run
 else
+  dryRun
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
+    run
   fi
 fi
 unset doIt
